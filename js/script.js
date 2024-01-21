f3c17f96a99ca9c419439ad5a852aa888e181cfa
Abigail Muthoni Wairi <89918147+Abbymuso1@users.noreply.github.com>
2024-01-21 16:20:20 +0300

    fix: Fixing the portfolio section
    Fixing the portfolio section to showcase more details on the projects

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 /* --------------------- typing animation ------------------------- */
 var typed = new Typed(".typing", {
-    strings:["Web Designer", "Web Developer", "Cgraphic Designer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
+    strings: ["Software Engineer", "Strathmore Graduate", "Full-Stack Developer"],
+    typeSpeed: 200,
+    BackSpeed: 60,
+    loop: true
 })
+
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("work__button")) {
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+});
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup)
+
+function portfolioItemDetails(portfolioItem){
+    console.log(portfolioItem);
+    document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".portfolio-item .portfolio-item-inner .work__img img").src;
+}
